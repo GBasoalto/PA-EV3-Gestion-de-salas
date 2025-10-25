32709ba931545f9b23b694f8440c69c485ec5649
Gonzalo Basoalto <gonzalobasoalto1@gmail.com>
2025-10-24 22:26:47 -0300

    Agregar archivos de proyecto.

---
+++ Gestion de salas/Data/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Gestion_de_salas.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            //identificar los modelos que se veran en la base de datos
+            //public DbSet<Herramienta> Herramientas { get; set; } EJEMPLO!!
+
+    }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) //CREAR SEMILLAS ACA
+        {
+            base.OnModelCreating(modelBuilder);
+            //EJEMPLOS DE SEMILLAS
+            //modelBuilder.Entity<Rol>().HasData(
+            //    new Rol { Id = 1, Nombre = "Administrador" },
+            //    new Rol { Id = 2, Nombre = "Empleado" }
+            //);
+
+            //modelBuilder.Entity<Herramienta>().HasData(
+            //    new Herramienta { Id = 1, Nombre = "Destornillador" },
+            //    new Herramienta { Id = 2, Nombre = "Taladro" },
+            //    new Herramienta { Id = 3, Nombre = "Martillo" }
+            //);
+        }
+    }
+}
